c4db8115dbfd3e85337cafcfcf9d1e82b9e9ff86
rohobie <ronitthakur267@gmail.com>
2026-03-21 03:34:57 +0530

    add functions to generate Code and return a session

--- backend/internal/types/session.go
+++ backend/internal/types/session.go
@@ -1,6 +1,10 @@
 package types
 
-import "sync"
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
 
 type Session struct {
 	Code      string
@@ -22,4 +26,27 @@ type Message struct {
 type Event struct {
 	Type string
 	Data interface{}
+}
+
+const codeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+func generateCode() string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+    b := make([]byte, 6)
+    for i := range b {
+        b[i] = codeChars[r.Intn(len(codeChars))]
+    }
+    return string(b)
+}
+
+func NewSession(owner *User) *Session {
+    return &Session{
+        Code:      generateCode(),
+        Owner:     owner,
+        Users:     map[string]*User{owner.UserID.String(): owner},
+        Timer:     &Timer{State: "idle"},
+        Perms:     false,
+        Chat:      make([]*Message, 0, 50),
+        Broadcast: make(chan Event, 32), 
+    }
 }
